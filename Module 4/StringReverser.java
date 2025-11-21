76e7ef31f2073f609837def51dd65fe844bc4339
Madison <madisonhauptmann@gmail.com>
2025-11-21 10:30:58 -0500

    Added StringReverser mh

---
+++ Module 4/StringReverser.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class StringReverser {
+    public static String reverse(char[] arr, int N) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            stack.push(arr[i]);
+        }
+
+        StringBuilder reversed = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
+        }
+
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        char[] arr = "hello".toCharArray();
+        int N = arr.length;
+
+        String result = reverse(arr, N);
+        System.out.println("Reversed: " + result);
+    }
+}
